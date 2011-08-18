237158537691e73f486e1ba412e3c98e9e64b61d
Bernd Schmidt <bernds@codesourcery.com>
2011-08-18 16:07:42 +0200

    Add a missing file.

---
+++ src/graphicsitemstypes.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by The qGo Project                                 *
+ *                                                                         *
+ *   This file is part of qGo.   					   *
+ *                                                                         *
+ *   qGo is free software: you can redistribute it and/or modify           *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *   or write to the Free Software Foundation, Inc.,                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*
+* GraphicsItems types for the board class
+*/
+#define RTTI_STONE 1001
+#define RTTI_MARK_SQUARE 1002
+#define RTTI_MARK_CIRCLE 1003
+#define RTTI_MARK_TRIANGLE 1004
+#define RTTI_MARK_CROSS 1005
+#define RTTI_MARK_TEXT 1006
+#define RTTI_MARK_NUMBER 1007
+#define RTTI_MARK_TERR 1008
+#define RTTI_MARK_OTHERLINE 1009
