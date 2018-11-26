40a4ea1e3889704fd0a6df343d188a96efdce471
Bernd Schmidt <bernds_cb1@t-online.de>
2018-11-26 18:01:41 +0100

    Privatize members that are not used elsewhere.

--- src/imagehandler.h
+++ src/imagehandler.h
@@ -27,7 +27,7 @@ class ImageHandler
 public:
 	ImageHandler();
 	~ImageHandler();
-	
+
 	void init(int size);
 	void rescale(int size);
 	static QPixmap* getBoardPixmap(QString );
@@ -37,6 +37,11 @@ public:
 	static QList<QPixmap> *getAlternateGhostPixmaps() { return altGhostPixmaps; }
 	void ghostImage(QImage *img);
 
+
+protected:
+	void scaleBoardPixmap(QPixmap *pix, int size);
+	
+private:
 	void icopy(int *im, QImage &qim, int w, int h);
 	void decideAppearance(WhiteDesc *desc, int size);
 	double getStripe(WhiteDesc &white, double bright, double z, int x, int y);
@@ -45,10 +50,6 @@ public:
 //	void paintWhiteStone2 (QImage &wi, int d, bool stripes);
 	void paintWhiteStone (QImage &wi, int d, int stone_render);//bool noShadow, bool stripes);
 
-protected:
-	void scaleBoardPixmap(QPixmap *pix, int size);
-	
-private:
 	QList<QPixmap> stonePixmaps, ghostPixmaps;
 	static QList<QPixmap> *altGhostPixmaps;
 	static QPixmap *tablePixmap;
