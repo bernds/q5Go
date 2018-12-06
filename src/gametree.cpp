07d35036bbcf4bda57c843bfc20d25c4878f8e34
Bernd Schmidt <bernds_cb1@t-online.de>
2018-12-06 18:24:08 +0100

    Remove unused includes.

--- src/gametree.cpp
+++ src/gametree.cpp
@@ -6,10 +6,7 @@
 #include "setting.h"
 #include "gametree.h"
 #include "globals.h"
-#include "mark.h"
 #include "imagehandler.h"
-#include "tip.h"
-#include "move.h"
 
 GameTree::GameTree(QWidget *parent, const char *name, QGraphicsScene* c)
   : QGraphicsView(c, parent)
