5272c3a2e249ff83f74c32cf3d8403f822f14b93
Bernd Schmidt <bernds_cb1@t-online.de>
2019-01-11 01:14:48 +0100

    Rewrite QGtp again to make it work on Windows
    QProcess and its communication channels behave differently on Linux and
    Windows.  On the latter in particular, it seems to be impossible to use
    both waitForReadyRead and the readyRead signal alternately, it seems it
    has to be one or the other.  But there were occasional strange issues
    on Linux as well.
    
    This rewrite changes everything in QGtp to use the signals, buffering
    up output and distributing it to our own receiver queue once a full
    line has been read.

--- src/qgtp.cpp
+++ src/qgtp.cpp
@@ -1,32 +1,13 @@
-/***************************************************************************
-qgtp.cpp  -  description
--------------------
-begin                : Thu Nov 29 2001
-copyright            : (C) 2001 by PALM Thomas , DINTILHAC Florian, HIVERT Anthony, PIOC Sebastien
-email                : 
-***************************************************************************/
-
-/***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************/
-
 #include <iostream>
-//#include <unistd.h>
+
 #include <stdlib.h>
+
 #include <QProcess>
+
 #include "defines.h"
 #include "qgtp.h"
 
-#ifdef Q_OS_WIN
-#include <Windows.h>
-#endif
-
-QGtp *Gtp_Controller::create_gtp (const QString &prog, int size, double komi, int hc, int level)
+QGtp *Gtp_Controller::create_gtp (const QString &prog, const QString &args, int size, double komi, int hc, int level)
 {
 	QGtp *g = new QGtp (m_parent, this, prog, size, komi, hc, level);
 	return g;
@@ -54,6 +35,8 @@ QGtp::QGtp(QWidget *parent, Gtp_Controller *c, const QString &prog, int size, fl
 	connect (m_process, fini, this, &QGtp::slot_finished);
 	connect (m_process, &QProcess::readyReadStandardError,
 		 this, &QGtp::slot_startup_messages);
+	connect (m_process, &QProcess::readyReadStandardOutput,
+		 this, &QGtp::slot_receive_stdout);
 
 	QStringList arguments = prog.split (QRegExp ("\\s+"));
 	QString filename = arguments.takeFirst();
@@ -64,103 +47,98 @@ QGtp::QGtp(QWidget *parent, Gtp_Controller *c, const QString &prog, int size, fl
 QGtp::~QGtp()
 {
 	if (m_process)
-		m_process->kill();     // not very clean (should use tryTerminate)
+		delete m_process;
 }
 
 void QGtp::slot_started ()
 {
-	connect (m_process, SIGNAL(readyReadStandardOutput()),
-		 this, SLOT(slot_startup_part2()));
-
-//	connect (m_process, SIGNAL(error()), this, SLOT(slot_later_error()) );
-
-	send_request ("protocol_version");
-	return;
+	send_request ("protocol_version", &QGtp::startup_part2);
 }
 
 void QGtp::slot_error (QProcess::ProcessError)
 {
+	m_receivers.clear ();
 	m_dlg.hide ();
 	m_controller->gtp_exited ();
-	return;
 }
 
 void QGtp::slot_abort_request (bool)
 {
-	send_request ("quit");
+	m_receivers.clear ();
+	quit ();
 	m_dlg.hide ();
 	m_controller->gtp_exited ();
-	return;
 }
 
 void QGtp::slot_startup_messages ()
 {
 	QString output = m_process->readAllStandardError ();
 	m_dlg.append (output);
-
 }
 
-void QGtp::slot_startup_part2 ()
+void QGtp::startup_part2 (const QString &response)
 {
-	disconnect (m_process, SIGNAL(readyReadStandardOutput()), 0, 0);
-	// m_dlg.hide ();
-
-	try {
-		// Get the response to the protocol version.
-		QString pv = getResponse ();
-		if (pv != "2")
-			throw invalid_response ();
-
-		setBoardsize (m_size);
-		clearBoard ();
-
-		if (knownCommand("level"))
-			setLevel (m_level);
-
-		setKomi (m_komi);
-		fixedHandicap(m_hc);
-		m_controller->gtp_startup_success ();
-	} catch (gtp_exception &e) {
-		m_controller->gtp_failure (e);
+	if (response != "2") {
+		m_controller->gtp_failure (wrong_protocol ());
 		quit ();
+		return;
 	}
+	send_request (QString ("boardsize ") + QString::number (m_size), &QGtp::startup_part3);
 }
 
-void QGtp::slot_receive_move ()
+void QGtp::startup_part3 (const QString &)
 {
-	disconnect (m_process, SIGNAL (readyReadStandardOutput()), 0, 0);
-	try {
-		QString response = getResponse ();
-		if (response.toLower () == "resign") {
-			m_controller->gtp_played_resign ();
-		} else if (response.toLower () == "pass") {
-			m_controller->gtp_played_pass ();
-		} else {
-			QChar sx = response[0];
-			response.remove (0, 1);
+	send_request ("clear_board", &QGtp::startup_part4);
+}
 
-			int i = sx.toLatin1() - 'A';
-			if (i > 7)
-				i--;
-			int j = response.toInt();
-			m_controller->gtp_played_move (i, m_size - j);
-		}
-	} catch (gtp_exception &e) {
-		m_controller->gtp_failure (e);
-		quit ();
+void QGtp::startup_part4 (const QString &)
+{
+	char komi[20];
+	sprintf (komi, "komi %.2f", m_komi);
+	t_receiver rcv = &QGtp::startup_part6;
+	if (m_hc > 2)
+		rcv = &QGtp::startup_part5;
+	send_request (komi, rcv);
+}
+
+void QGtp::startup_part5 (const QString &)
+{
+	send_request (QString ("fixed_handicap ") + QString::number (m_hc), &QGtp::startup_part6);
+}
+
+void QGtp::startup_part6 (const QString &)
+{
+	m_controller->gtp_startup_success ();
+}
+
+void QGtp::receive_move (const QString &move)
+{
+	if (move.toLower () == "resign") {
+		m_controller->gtp_played_resign ();
+	} else if (move.toLower () == "pass") {
+		m_controller->gtp_played_pass ();
+	} else {
+		QChar sx = move[0];
+
+		int i = sx.toLatin1() - 'A';
+		if (i > 7)
+			i--;
+		int j = move.mid (1).toInt();
+		m_controller->gtp_played_move (i, m_size - j);
 	}
 }
 
 void QGtp::played_move (stone_color col, int x, int y)
 {
-	disconnect (m_process, SIGNAL (readyReadStandardOutput()), 0, 0);
 	if (x >= 8)
 		x++;
 	try {
+		char req[20];
+		sprintf (req, "%c%d", 'A' + x, m_size - y);
 		if (col == black)
-			playblack ('A' + x, m_size - y);
+			send_request ("play black " + QString (req));
 		else
-			playwhite ('A' + x, m_size - y);
+			send_request ("play white " + QString (req));
 	} catch (gtp_exception &e) {
 		m_controller->gtp_failure (e);
 		quit ();
@@ -169,12 +147,11 @@ void QGtp::played_move (stone_color col, int x, int y)
 
 void QGtp::played_move_pass (stone_color col)
 {
-	disconnect (m_process, SIGNAL (readyReadStandardOutput()), 0, 0);
 	try {
 		if (col == black)
-			playblackPass ();
+			send_request ("play black pass");
 		else
-			playwhitePass ();
+			send_request ("play white pass");
 	} catch (gtp_exception &e) {
 		m_controller->gtp_failure (e);
 		quit ();
@@ -183,14 +160,10 @@ void QGtp::played_move_pass (stone_color col)
 
 void QGtp::request_move (stone_color col)
 {
-	disconnect (m_process, SIGNAL (readyReadStandardOutput()), 0, 0);
-	connect (m_process, SIGNAL (readyReadStandardOutput()),
-		 this, SLOT (slot_receive_move()));
-
 	if (col == black)
-		send_request ("genmove black");
+		send_request ("genmove black", &QGtp::receive_move);
 	else
-		send_request ("genmove white");
+		send_request ("genmove white", &QGtp::receive_move);
 }
 
 /* Code */
@@ -203,113 +176,70 @@ void QGtp::slot_finished (int exitcode, QProcess::ExitStatus status)
 }
 
 
-/* Function:  wait for a go response
-* Arguments: none
-* Fails:     never
-* Returns:   nothing
-*/
-QString QGtp::waitResponse()
+/* QProcess communication has been a source of problems - so we don't use the readyRead
+   slots directly to receive GTP responses.  Instead, we have this intermediate function
+   to collect ouput and pass along full output lines to receivers.  This way we can even
+   queue up multiple commands at once.  */
+void QGtp::slot_receive_stdout ()
 {
-	m_process->waitForReadyRead ();
-	return getResponse ();
-}
+	m_buffer += m_process->readAllStandardOutput ();
 
-QString QGtp::getResponse ()
-{
-	QString output;
-	do {
-		while (m_process->canReadLine ()) {
-			output = m_process->readLine ();
-
-			m_dlg.textEdit->setTextColor (Qt::red);
-			m_dlg.append (output);
-			m_dlg.textEdit->setTextColor (Qt::black);
-			output = output.trimmed ();
-			if (!output.isEmpty ())
-				break;
+	for (;;) {
+		/* Clear empty lines at the front of the buffer.  */
+		int last_lf = -1;
+		int pos = 0;
+		while (pos < m_buffer.length () && m_buffer[pos].isSpace ()) {
+			if (m_buffer[pos] == '\n')
+				last_lf = pos;
+			pos++;
 		}
-		if (output.isEmpty ()) {
-			/* @@@
-			   This seems to happen every now and then with Leela Zero.
-			   It's unclear why, but there's little we can do except
-			   wait for another round.  */
-			qDebug () << "empty output from GTP program";
-			m_process->waitForReadyRead ();
-		}
-	} while (output.isEmpty ());
+		if (last_lf >= 0)
+			m_buffer = m_buffer.mid (last_lf + 1);
+		if (m_receivers.isEmpty ())
+			return;
 
-	int n_tws = 0;
-	int len = output.length ();
-	while (len > n_tws && output[len - n_tws - 1].isSpace ())
-		n_tws++;
+		int idx = m_buffer.indexOf ("\n");
+		if (idx < 0)
+			return;
 
-	qDebug() << "** QGtp::getResponse(): " << output << "\n";
-;
-	output.chop (n_tws);
-	len -= n_tws;
-
-#if 0
-	std::cout << "R: " << output.toStdString() << "\n";
-#endif
-	if (output[0] != '=')
-		throw invalid_response ();
-	output.remove (0, 1);
-	len--;
-	int n_digits = 0;
-	while (n_digits < len && output[n_digits].isDigit ())
-		n_digits++;
-	while (n_digits < len && output[n_digits].isSpace ())
-		n_digits++;
-	QString cmd_nr = output.left (n_digits);
-	output.remove (0, n_digits);
-
-#if 0
-	std::cout << cmd_nr.toStdString () << " X " << output.toStdString () << "\n";
-#endif
-	if (cmd_nr.toInt () != req_cnt - 1)
-		throw invalid_response ();
-	while (m_process->canReadLine ()) {
-		QString l = m_process->readLine ();
+		QString output = m_buffer.left (idx).trimmed ();
+		m_buffer = m_buffer.mid (idx + 1);
 
 		m_dlg.textEdit->setTextColor (Qt::red);
-		m_dlg.append (l);
+		m_dlg.append (output);
 		m_dlg.textEdit->setTextColor (Qt::black);
-		l = l.trimmed ();
-		if (!l.isEmpty ())
-			throw invalid_response ();
+
+		if (output[0] != '=') {
+			m_controller->gtp_failure (invalid_response ());
+			quit ();
+			return;
+		}
+		output.remove (0, 1);
+		int len = output.length ();
+		int n_digits = 0;
+		while (n_digits < len && output[n_digits].isDigit ())
+			n_digits++;
+		while (n_digits < len && output[n_digits].isSpace ())
+			n_digits++;
+		int cmd_nr = output.left (n_digits).toInt ();
+		QMap<int, t_receiver>::const_iterator map_iter = m_receivers.constFind (cmd_nr);
+		if (map_iter == m_receivers.constEnd ()) {
+			m_controller->gtp_failure (invalid_response ());
+			quit ();
+			return;
+		}
+		t_receiver rcv = *map_iter;
+		m_receivers.remove (cmd_nr);
+		output.remove (0, n_digits);
+		if (rcv != nullptr)
+			(this->*rcv) (output);
 	}
-	return output;
 }
 
-/****************************
-* Program identity.        *
-****************************/
-
-/* Function:  Report the name of the program.
-* Arguments: none
-* Fails:     never
-* Returns:   program name
-*/
-QString QGtp::name ()
-{
-	send_request ("name");
-	return waitResponse ();
-}
-
-/* Function:  Report protocol version.
-* Arguments: none
-* Fails:     never
-* Returns:   protocol version number
-*/
-int QGtp::protocolVersion ()
-{
-	send_request ("protocol_version");
-	return waitResponse().toInt ();
-}
-
-void QGtp::send_request(const QString &s)
+void QGtp::send_request(const QString &s, t_receiver rcv)
 {
 	qDebug() << "send_request -> " << req_cnt << " " << s << "\n";
+	m_receivers[req_cnt] = rcv;
 #if 1
 	m_dlg.textEdit->setTextColor (Qt::blue);
 	m_dlg.append (s);
@@ -321,186 +251,9 @@ void QGtp::send_request(const QString &s)
 	req_cnt++;
 }
 
-/****************************
-* Administrative commands. *
-****************************/
-
-/* Function:  Quit
-* Arguments: none
-* Fails:     never
-* Returns:   nothing
-*/
-void
-QGtp::quit ()
+void QGtp::quit ()
 {
+	disconnect (m_process, &QProcess::readyReadStandardOutput, nullptr, nullptr);
+	disconnect (m_process, &QProcess::readyReadStandardError, nullptr, nullptr);
 	send_request ("quit");
 }
-
-#if 0
-/* Function:  Report the version number of the program.
-* Arguments: none
-* Fails:     never
-* Returns:   version number
-*/
-QString QGtp::version ()
-{
-	send_request ("version");
-	return waitResponse();
-}
-#endif
-
-/***************************
-* Setting the board size. *
-***************************/
-
-/* Function:  Set the board size to NxN.
-* Arguments: integer
-* Fails:     board size outside engine's limits
-* Returns:   nothing
-*/
-void
-QGtp::setBoardsize (int size)
-{
-	send_request (QString ("boardsize ") + QString::number (size));
-	waitResponse();
-}
-
-/***********************
-* Clearing the board. *
-***********************/
-
-/* Function:  Clear the board.
-* Arguments: none
-* Fails:     never
-* Returns:   nothing
-*/
-
-void QGtp::clearBoard ()
-{
-	send_request ("clear_board");
-	waitResponse();
-}
-
-/***************************
-* Setting.           *
-***************************/
-
-/* Function:  Set the komi.
-* Arguments: float
-* Fails:     incorrect argument
-* Returns:   nothing
-*/
-void
-QGtp::setKomi(float f)
-{
-	char komi[20];
-	sprintf (komi, "komi %.2f", f);
-	send_request (komi);
-	waitResponse();
-}
-
-/* Function:  Set the playing level.
-* Arguments: int
-* Fails:     incorrect argument
-* Returns:   nothing
-*/
-void QGtp::setLevel (int level)
-{
-	send_request (QString ("level ") + QString::number (level));
-	waitResponse();
-}
-/******************
-* Playing moves. *
-******************/
-
-/* Function:  Play a black stone at the given vertex.
-* Arguments: vertex
-* Fails:     invalid vertex, illegal move
-* Returns:   nothing
-*/
-void
-QGtp::playblack (char c , int i)
-{
-	char req[20];
-	sprintf (req, "play black %c%d", c, i);
-	send_request (req);
-	waitResponse();
-}
-
-/* Function:  Black pass.
-* Arguments: None
-* Fails:     never
-* Returns:   nothing
-*/
-void
-QGtp::playblackPass ()
-{
-	send_request ("play black pass");
-	waitResponse();
-}
-
-/* Function:  Play a white stone at the given vertex.
-* Arguments: vertex
-* Fails:     invalid vertex, illegal move
-* Returns:   nothing
-*/
-void
-QGtp::playwhite (char c, int i)
-{
-	char req[20];
-	sprintf (req, "play white %c%d", c, i);
-	send_request (req);
-	waitResponse();
-}
-
-/* Function:  White pass.
-* Arguments: none
-* Fails:     never
-* Returns:   nothing
-*/
-void
-QGtp::playwhitePass ()
-{
-	send_request ("play white pass");
-	waitResponse();
-}
-
-/* Function:  Set up fixed placement handicap stones.
-* Arguments: number of handicap stones
-* Fails:     invalid number of stones for the current boardsize
-* Returns:   list of vertices with handicap stones
-*/
-void
-QGtp::fixedHandicap (int handicap)
-{
-	if (handicap < 2) 
-		return;
-
-	send_request (QString ("fixed_handicap ") + QString::number (handicap));
-	waitResponse();
-}
-
-#if 0
-/* Function:  Undo last move
-* Arguments: int
-* Fails:     If move pointer is 0
-* Returns:   nothing
-*/
-void QGtp::undo (int i)
-{
-	send_request (QString ("undo ") + QString::number (i));
-	waitResponse();
-}
-#endif
-
-/* Function:  evaluate wether a command is known
-* Arguments: command word
-* Fails:     never
-* Returns:   true or false
-*/
-bool QGtp::knownCommand (QString s)
-{
-	send_request (QString ("known_command ") + s);
-	return waitResponse() == "true";
-}
-
--- src/qgtp.h
+++ src/qgtp.h
@@ -1,29 +1,10 @@
-/***************************************************************************
-                          qgtp.h  -  description
-                             -------------------
-    begin                : Thu Nov 29 2001
-    copyright            : (C) 2001 by PALM Thomas , DINTILHAC Florian, HIVERT Anthony, PIOC Sebastien
-    email                : 
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef QGTP_H
 #define QGTP_H
 
-#include <qapplication.h>
 #include <QProcess>
 
 #include "goboard.h"
 #include "textview.h"
-//#include "global.h"
 
 class gtp_exception : public std::exception
 {
@@ -33,16 +14,23 @@ public:
 
 class process_timeout : public gtp_exception
 {
- public:
-  process_timeout () { }
-  QString errmsg () const { return QObject::tr ("Program timed out"); }
+public:
+	process_timeout () { }
+	QString errmsg () const { return QObject::tr ("Program timed out"); }
 };
 
 class invalid_response : public gtp_exception
 {
- public:
-  invalid_response () { }
-  QString errmsg () const { return QObject::tr ("Invalid response from program"); }
+public:
+	invalid_response () { }
+	QString errmsg () const { return QObject::tr ("Invalid response from program"); }
+};
+
+class wrong_protocol : public gtp_exception
+{
+public:
+	wrong_protocol () { }
+	QString errmsg () const { return QObject::tr ("Program reported unsupported protocol version"); }
 };
 
 #define IGTP_BUFSIZE 2048    /* Size of the response buffer */
@@ -67,15 +55,13 @@ public:
 	virtual void gtp_failure (const gtp_exception &) = 0;
 };
 
-/**
-  *@author PALM Thomas , DINTILHAC Florian, HIVERT Anthony, PIOC Sebastien
-  */
-
 class QGtp : public QObject
 {
 	Q_OBJECT
 
 	QProcess *m_process;
+	QString m_buffer;
+
 	TextView m_dlg;
 	Gtp_Controller *m_controller;
 
@@ -84,13 +70,26 @@ class QGtp : public QObject
 	int m_hc;
 	int m_level;
 
+	typedef void (QGtp::*t_receiver) (const QString &);
+	QMap <int, t_receiver> m_receivers;
+
+	/* Number of the next request.  */
+	int req_cnt;
+	void send_request(const QString &, t_receiver = nullptr);
+
+	void startup_part2 (const QString &);
+	void startup_part3 (const QString &);
+	void startup_part4 (const QString &);
+	void startup_part5 (const QString &);
+	void startup_part6 (const QString &);
+	void receive_move (const QString &);
+
 public slots:
 	void slot_started ();
 	void slot_finished (int exitcode, QProcess::ExitStatus status);
 	void slot_error (QProcess::ProcessError);
-	void slot_receive_move ();
+	void slot_receive_stdout ();
 	void slot_startup_messages ();
-	void slot_startup_part2 ();
 	void slot_abort_request (bool);
 
 // void slot_stateChanged(QProcess::ProcessState);
@@ -104,56 +103,7 @@ public:
 	void played_move_pass (stone_color col);
 	void played_move_resign (stone_color col);
 
-	/****************************
- 	* Administrative commands. *
- 	****************************/
 	void quit ();
-
-	/****************************
- 	* Program identity.        *
- 	****************************/
-	QString name ();
-	int protocolVersion ();
-	QString version ();
-
-	/***************************
- 	* Setting the board size. *
- 	***************************/
-	void setBoardsize (int size);
-
-	/***************************
-	* Clearing the board.     *
- 	***************************/
-	void clearBoard ();
-
-	/************
- 	* Setting. *
- 	************/
-	void setKomi (float k);
-	void setLevel (int level);
-
-	/******************
- 	* Playing moves. *
- 	******************/
-	void playblack (char c, int j);
-	void playblackPass ();
-	void playwhite (char c, int j);
-	void playwhitePass ();
-	void fixedHandicap (int handicap);
-
-	bool knownCommand(QString s);
-
-private:
-
-	/* Number of the request */
-	int req_cnt;
-
-	void send_request(const QString &);
-	QString waitResponse();
-	QString getResponse();
 };
-
-
-
 
 #endif
