e929e15df71c7b0aebcc433d469d7d594280a267
Bernd Schmidt <bernds_cb1@t-online.de>
2019-02-05 21:40:35 +0100

    Update mark extra data to unsigned short
    We'll use these for move numbers rather than using count_maps, and they
    can go beyond 256.

--- src/goboard.h
+++ src/goboard.h
@@ -22,7 +22,7 @@ enum stone_color
 
 enum class stone_type { live, seki, dead, var };
 enum class mark { none = 0, move, triangle, circle, square, plus, cross, text, num, letter, dead, seki, terr, falseeye };
-typedef unsigned char mextra;
+typedef unsigned short mextra;
 
 class go_board
 {
