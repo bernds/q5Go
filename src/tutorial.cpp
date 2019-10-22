8a85b353e03321bd6ee880f08c96a75a9d82f947
Bernd Schmidt <bernds_cb1@t-online.de>
2019-10-22 20:42:47 +0200

    Add an openings tutorial

--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -18,13 +18,14 @@ struct tutorial {
 	int lines;
 };
 
-static std::array<tutorial, 5> tutorials =
+static std::array<tutorial, 6> tutorials =
 {
 	tutorial { QObject::tr ("The Rules of Go"), ":/Tutorials/sgfs/rules.sgf", 12 },
 	tutorial { QObject::tr ("Life and Death"), ":/Tutorials/sgfs/lnd.sgf", 12 },
 	tutorial { QObject::tr ("Basic Tactics"), ":/Tutorials/sgfs/tactics.sgf", 12 },
 	tutorial { QObject::tr ("Connections"), ":/Tutorials/sgfs/connections.sgf", 12 },
-	tutorial { QObject::tr ("Captures"), ":/Tutorials/sgfs/captures.sgf", 12 }
+	tutorial { QObject::tr ("Captures"), ":/Tutorials/sgfs/captures.sgf", 12 },
+	tutorial { QObject::tr ("Openings"), ":/Tutorials/sgfs/openings.sgf", 12 }
 };
 
 Tutorial_Slideshow::Tutorial_Slideshow (QWidget *parent)
