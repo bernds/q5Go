e15e14a6e58d05adbe9d3c21e0d89bdded669b57
Bernd Schmidt <bernds_cb1@t-online.de>
2020-04-28 02:06:19 +0200

    Identify binaries built by GitHub actions in the version string

--- src/config.h
+++ src/config.h
@@ -4,9 +4,18 @@
 #define PACKAGE1 "q5goClient"
 
 /* Version number of package */
-#define VERSION "1.1"
+#define VERSION_1 "1.1"
+
+#ifdef GITHUB_CI_BUILD
+#define VERSION VERSION_1 " (GitHub action CI build on " __DATE__ ")"
+#define CIBUILD "<p>Please note that this is not an official release build, but a GitHub CI build artifact.</p>"
+#else
+#define CIBUILD
+#define VERSION VERSION_1
+#endif
 
 #define NEWVERSIONWARNING "<p>Welcome to version " VERSION " of q5go.</p>\n" \
+  CIBUILD \
   "<p>This version mostly contains tweaks and improvements to existing features.</p>" \
   "<p>Working with very large SGF files should be faster, and there are some additional options for time warnings in online games.</p>" \
   "<p>For a full list of changes, please refer to the README.</p>" \
