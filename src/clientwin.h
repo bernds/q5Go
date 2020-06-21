c74a67d91361ed31cc4e6e41282834e01d1d0e60
Bernd Schmidt <bernds_cb1@t-online.de>
2020-06-21 22:32:22 +0200

    Add a specialization for hashing QString
    Required for older versions of Qt.

--- src/clientwin.h
+++ src/clientwin.h
@@ -31,6 +31,21 @@ struct sendBuf
 	sendBuf (QString text, bool echo=true) { txt = text; localecho = echo; }
 };
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+#include <QHash>
+
+namespace std
+{
+    template<> struct hash<QString>
+    {
+        std::size_t operator() (const QString& s) const noexcept
+        {
+            return (size_t)qHash (s);
+        }
+    };
+}
+#endif
+
 class ClientWindow : public QMainWindow, public Ui::ClientWindowGui
 {
 	Q_OBJECT
